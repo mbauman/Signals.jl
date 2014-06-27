9f60dec7d598977795b66be9762643665b0791c4
Matt Bauman <mbauman@gmail.com>
2014-06-27 18:04:41 -0400

    Add interpolation for Signals using Grid.jl

--- src/Signals.jl
+++ src/Signals.jl
@@ -1,16 +1,20 @@
 module Signals
 
+# Functions Signals extend
 import Base: elsize
 import ArrayViews: view
 import Sparklines: spark
+import Grid: interp
 
 export Signal,
        RegularSignal,
        regularize,
        isregular,
        ishomogeneous,
-       channeltypes
+       channeltypes,
+       interp
 
 include("signal.jl")
+include("interpolation.jl")
 
 end # module
---
+++ src/interpolation.jl
@@ -0,0 +1,31 @@
+# Interpolation between different time bases
+
+# Brainstorm: How do I want to do interpolation? I think I want something like
+# interp1 / qinterp1. What about Grid.jl? Is that kind of dynamic interpolation
+# worth investigating?  I mean, maybe a signal could become:
+#      typealias InterpSignal{N, T<:AbstractInterpGrid, S<:AbstractInterpGrid} Signal{N,T,S}
+# But perhaps simply returning the result is all we need here.
+import Grid
+
+function Grid.interp{N,T,S}(s::RegularSignal{N,T,S}, ti::AbstractVector)
+    vi = Array(S,N)
+    # Using a Regular Grid is a little awkward, but more performant; it assumes the original basis is 1:N
+    t = s.time
+    # So convert ti to "indices" relative to s.time
+    tidxs = (ti-t[1])/step(t) + 1
+    for (i,chan) in enumerate(s)
+        gi = Grid.InterpGrid(chan, Grid.BCnan, Grid.InterpLinear)
+        vi[i] = gi[tidxs]
+    end
+    Signal(ti, vi)
+end
+
+function Grid.interp{N,T,S}(s::Signal{N,T,S}, ti::AbstractVector)
+    vi = Array(S,N)
+    t = s.time
+    for (i,chan) in enumerate(s)
+        gi = Grid.InterpIrregular(t, chan, Grid.BCnan, Grid.InterpLinear)
+        vi[i] = gi[ti]
+    end
+    Signal(ti, vi)
+end
