68cf3fcbf67c0b2aa55f7035349e25f1721b5315
Matt Bauman <mbauman@gmail.com>
2014-07-15 17:32:45 -0400

    Less futzing with SIUnits for interp
    Requires timholy/Grid.jl/pull/35

--- src/interpolation.jl
+++ src/interpolation.jl
@@ -3,38 +3,20 @@
 import Grid
 import ArrayViews: ArrayView
 
-# Hacks to get arrays of unitful quantities to behave. This is a pain.
-datatype{T<:SIUnits.SIQuantity}(::Type{T}) = T.parameters[1]
-function stripunits{T<:SIUnits.SIQuantity}(x::Array{T})
-    ptr = reinterpret(Ptr{datatype(T)},pointer(x))
-    out = pointer_to_array(ptr,length(x))
-    reshape(out, size(x))
-end
-stripunits(x::Union(SIUnits.SIQuantity, SIUnits.SIRanges)) = x.val
-
 # Can't parameterize RegularSignal due to typealias issues (#7453: #2552, #6721)
-function Grid.interp{N,T<:Range,S}(sig::Signal{N,T,S}, ti::AbstractVector)
-    vi = S<:ArrayView ? Array(Array{eltype(S),1},N) : Array(S,N)
+function Grid.interp{N,T<:Range,S<:SecondT}(sig::Signal{N,T}, ti::AbstractVector{S})
     # Using a Regular Grid is a little awkward, but more performant; it assumes the original basis is 1:N
     t = sig.time
     # So convert ti to "indices" relative to sig.time
-    # TODO: This is a terrible hack. SIUnits thinks t/step(t) is still in seconds. https://github.com/Keno/SIUnits.jl/issues/27
+    # TODO: SIUnits thinks that the vectorized (ti-t[1])/step(t) is in seconds. https://github.com/Keno/SIUnits.jl/issues/27
     tidxs = Array(Float64, length(ti))
     for i=1:length(ti) tidxs[i] = (ti[i] - t[1])/step(t) + 1; end
 
-    for (i,chan) in enumerate(sig)
-        gi = Grid.InterpGrid(chan, Grid.BCnan, Grid.InterpLinear)
-        vi[i] = gi[tidxs]
-    end
+    vi = [Grid.InterpGrid(float(c), Grid.BCnan, Grid.InterpLinear)[tidxs] for c in sig]
     Signal(ti, vi)
 end
 
-function Grid.interp{N,T,S}(sig::Signal{N,T,S}, ti::AbstractVector)
-    vi = S<:ArrayView ? Array(Array{eltype(S),1},N) : Array(S,N)
-    t = sig.time
-    for (i,chan) in enumerate(sig)
-        gi = Grid.InterpIrregular(stripunits(t), chan, Grid.BCnan, Grid.InterpLinear)
-        vi[i] = gi[stripunits(ti)]
-    end
+function Grid.interp{N,T,S<:SecondT}(sig::Signal{N,T}, ti::AbstractVector{S})
+    vi = [Grid.InterpIrregular(sig.time, float(c), Grid.BCnan, Grid.InterpLinear)[ti] for c in sig]
     Signal(ti, vi)
 end
