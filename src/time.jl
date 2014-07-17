30a17244442fc79701715188a751baed580e0b6f
Matt Bauman <mbauman@gmail.com>
2014-07-17 10:39:45 -0400

    Fix windowing error
    Use :next for lower bound, :previous for upper bound in within

--- src/time.jl
+++ src/time.jl
@@ -29,8 +29,8 @@ after(sig::Signal, t::SecondT) = after(sig, time2idx(sig, t, :next))
 after(sig::Signal, i::Real)    = within(sig, i, length(sig.time))
 
 within(sig::Signal, i::(Real, Real))          = within(sig, t[1], t[2])
-within(sig::Signal, t::(SecondT, SecondT))    = within(sig, time2idx(sig, t[1], :previous), time2idx(sig, t[2], :next))
-within(sig::Signal, t1::SecondT, t2::SecondT) = within(sig, time2idx(sig, t1,   :previous), time2idx(sig, t2,   :next))
+within(sig::Signal, t::(SecondT, SecondT))    = within(sig, time2idx(sig, t[1], :next), time2idx(sig, t[2], :previous))
+within(sig::Signal, t1::SecondT, t2::SecondT) = within(sig, time2idx(sig, t1,   :next), time2idx(sig, t2,   :previous))
 function within(sig::Signal, i1::Real, i2::Real) # The real (and only) workhorse
     Signal(timewithin(sig, i1, i2), chanswithin(i1, i2))
 end
@@ -41,16 +41,16 @@ timebefore(sig::Signal, i::Real)    = timewithin(sig, 1, i)
 timeafter(sig::Signal, t::SecondT)  = timeafter(sig, time2idx(sig, t, :next))
 timeafter(sig::Signal, i::Real)     = timewithin(sig, i, length(sig.time))
 timewithin(sig::Signal, i::(Real, Real))          = timewithin(sig, t[1], t[2])
-timewithin(sig::Signal, t::(SecondT, SecondT))    = timewithin(sig, time2idx(sig, t[1], :previous), time2idx(sig, t[2], :next))
-timewithin(sig::Signal, t1::SecondT, t2::SecondT) = timewithin(sig, time2idx(sig, t1,   :previous), time2idx(sig, t2,   :next))
+timewithin(sig::Signal, t::(SecondT, SecondT))    = timewithin(sig, time2idx(sig, t[1], :next), time2idx(sig, t[2], :previous))
+timewithin(sig::Signal, t1::SecondT, t2::SecondT) = timewithin(sig, time2idx(sig, t1,   :next), time2idx(sig, t2,   :previous))
 timewithin(sig::Signal, i1::Real, i2::Real)       = sig.time[i1:i2]
 chansbefore(sig::Signal, t::SecondT) = chansbefore(sig, time2idx(sig, t, :previous))
 chansbefore(sig::Signal, i::Real)    = chanswithin(sig, 1, i)
 chansafter(sig::Signal, t::SecondT)  = chansafter(sig, time2idx(sig, t, :next))
 chansafter(sig::Signal, i::Real)     = chanswithin(sig, i, length(sig.time))
 chanswithin(sig::Signal, i::(Real, Real))          = chanswithin(sig, t[1], t[2])
-chanswithin(sig::Signal, t::(SecondT, SecondT))    = chanswithin(sig, time2idx(sig, t[1], :previous), time2idx(sig, t[2], :next))
-chanswithin(sig::Signal, t1::SecondT, t2::SecondT) = chanswithin(sig, time2idx(sig, t1,   :previous), time2idx(sig, t2,   :next))
+chanswithin(sig::Signal, t::(SecondT, SecondT))    = chanswithin(sig, time2idx(sig, t[1], :next), time2idx(sig, t[2], :previous))
+chanswithin(sig::Signal, t1::SecondT, t2::SecondT) = chanswithin(sig, time2idx(sig, t1,   :next), time2idx(sig, t2,   :previous))
 chanswithin(sig::Signal, i1::Real, i2::Real)       = [c[i1:i2] for c in sig]
 
 # Windowing a regular signal returns a signal of a signals: one for each channel
@@ -86,7 +86,6 @@ function window{N,T,S,R<:SecondT}(sig::Signal{N,T,S}, at::AbstractVector{R}, wit
     # Set number of indexes within each window, about time
     r1, r2 = within
     r = r1:r2
-    @show sig.time[time2idx(sig, at[1], :nearest)+r] - at[1]
     cs = Signal[(i = time2idx(sig, t, :nearest); Signal(sig.time[i+r] - t, chanswithin(sig,i+r1,i+r2))) for t in at]
     signal(at, cs)
 end
@@ -95,7 +94,6 @@ function window{N,T,S,R<:Real}(sig::Signal{N,T,S}, at::AbstractVector{R}, within
     r1, r2 = within
     r = r1:r2
     cs = Signal[Signal(sig.time[i+r] - sig.time[i], chanswithin(sig,i+r1, i+r2)) for i in at]
-    
     signal(sig.time[at], cs)
 end
 function window{N,T,S,R<:SecondT}(sig::Signal{N,T,S}, at::AbstractVector{R}, within::(SecondT, SecondT), channels=1:length(sig))
--- test/window.jl
+++ test/window.jl
@@ -1,12 +1,11 @@
 # Test windowing.
 
 ## Regular Signals:
-for loc in ([20,30,40], [2s,3s,4s], [2.04s,2.95s,4.04s]), win in ((-1s,1s), (-10,10))
+for loc in ([20,30,40], [2s,3s,4s]), win in ((-1s,1s), (-10,10), (-1.04s, 1.04s))
     t = 0.1s:0.1s:10.0s
     sig = signal(t, collect(1:100))
-    idxs = [20,30,40]
-    reps = window(sig, idxs, (-1s, 1s))
-    @test reps.time == t[idxs]
+    reps = window(sig, loc, win)
+    @test reps.time == [2.0s,3.0s,4.0s]
     @test reps[1].time == -1s:0.1s:1s
     @test length(reps) == 1
     @test length(reps[1]) == 3
@@ -17,13 +16,11 @@ end
 
 ## Irregular signals are tougher to test the edge-cases (with 4 different cases)
 # Start with basic behavior like Regular Signals
-
-for loc in ([20,30,40], [2s,3s,4s], [2.04s,2.95s,4.04s]), win in ((-1s,1s), (-10,10))
+for loc in ([20,30,40], [2s,3s,4s]), win in ((-1s,1s), (-10,10), (-1.04s, 1.04s))
     t = collect(0.1s:0.1s:10.0s)
     sig = signal(t, collect(1:100))
-    idxs = [20,30,40]
-    reps = window(sig, idxs, (-1s, 1s))
-    @test reps.time == t[idxs]
+    reps = window(sig, loc, win)
+    @test reps.time == [2.0s,3.0s,4.0s]
     @test length(reps) == 1
     @test length(reps[1]) == 3
     # Unlike regular signals, reps[1] is an array, not a Signal.
